222bfa900564fad0e8a9d453687bf39bfd08c5a7
yuu <yuu3@users.noreply.github.com>
2016-03-17 16:07:19 +0900

    Fix Not dependent to runtime directory

--- conv.sh
+++ conv.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-pandoc -s --self-contained -t html5 -c css/github.css $@
+script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+pandoc -s --self-contained -t html5 -c $script_dir/css/github.css $@
